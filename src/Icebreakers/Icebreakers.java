5eaebb230badd3f6d438c7f3506410b830d4af66
Julie Chen <juchen@scusd.net>
2020-08-03 22:53:24 -0700

    revamp Initial assignment

---
+++ src/Icebreakers/Icebreakers.java
@@ -0,0 +1,17 @@
+package Icebreakers;
+
+public class Icebreakers {
+	public static void main(String[] args) {
+		/** Enter your answers to the icebreakers below:
+		 
+		 
+		 
+		
+
+
+		  */
+		System.out.println("Welcome to APCSA.");
+
+	}
+}
+
